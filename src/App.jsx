a0f65fb6d206b6da31e4e467f13e83e5b5037203
vishwjeet <vishwjeetmanwar71@gmail.com>
2026-09-09 15:02:22 +0530

    feat: initial app logic and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import "./App.css";
+import { useState } from "react";
+
+// components
+import useDisplayMessage from "./components/DisplayMessages";
+
+// context
+import ProjectContext from "./context/ProjectContext";
+import { useContext } from "react";
+
+// pages
+import MainLandingPage from "./pages/MainLandingPage";
+import BackendLandingPage from "./pages/BackendLandingPage";
 
 function App() {
-  return
+
+  const [portNumber, setPortNumber] = useState(8000);
+  const [backendStatus, setBackendStatus] = useState(false);
+
+  return (
+    <>
+    <ProjectContext.Provider value={portNumber}>
+      {backendStatus ? (
+        <MainLandingPage />
+      ) : (
+        <BackendLandingPage />
+      )}
+      </ProjectContext.Provider>
+    </>
+  );
 }
 
 export default App;
+
+export const useProjectContext = () => {
+  return useContext(ProjectContext);
+}
