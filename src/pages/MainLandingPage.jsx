8db4e473b389bedd63732fb7b014ab8ed62574e6
vishwjeet <vishwjeetmanwar71@gmail.com>
2026-09-10 11:44:46 +0530

    feat: add initial landing page structure

---
+++ src/pages/MainLandingPage.jsx
@@ -0,0 +1,10 @@
+export default function MainLandingPage() {
+
+    return (
+        <>
+            <h2>Welcome To LinguSync</h2>
+            <h3>Your Local Video Dubbing system</h3>
+        </>
+    );
+
+}
