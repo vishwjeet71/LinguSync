c3e68ee0ffc6ab68da78bcea3c0cae73ae7f23df
vishwjeet <vishwjeetmanwar71@gmail.com>
2026-09-12 14:49:50 +0530

    feat: integrate main and backend landing pages in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,15 +17,24 @@ function App() {
   const [portNumber, setPortNumber] = useState(8000);
   const [backendStatus, setBackendStatus] = useState(false);
 
+  // messages
+  const { currentMessage, setDisplayMessage } = useDisplayMessage();
+
   return (
     <>
-    <ProjectContext.Provider value={portNumber}>
-      {backendStatus ? (
-        <MainLandingPage />
-      ) : (
-        <BackendLandingPage />
-      )}
+      <ProjectContext.Provider value={
+        { portNumber, setDisplayMessage }
+      }>
+        {backendStatus ? (<MainLandingPage />) : (
+          <BackendLandingPage
+            setBackendStatus={setBackendStatus}
+            setPortNumber={setPortNumber}
+          />
+        )}
       </ProjectContext.Provider>
+      <div>
+        {currentMessage}
+      </div>
     </>
   );
 }
